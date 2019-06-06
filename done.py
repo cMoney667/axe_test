55bba38aac2a6e710a7fc0c5a891c22deeffaff9
Collin OConnell <ococncol@gmail.com>
2019-06-06 10:53:31 -0400

    init commit should be working now

---
+++ done.py
@@ -0,0 +1,13 @@
+import os
+
+print('Please select the folder')
+foldername = input()
+done_file = open(f'{foldername}/{foldername}-adascan.docx', 'w')
+print('What is the citys name?')
+city_name = input()
+done_file.write(f"{city_name.title()} - ADA Scan\n\n")
+for filename in os.listdir(foldername):
+  with open(foldername+'/'+filename, 'r') as f:
+    for line in f.readlines():
+      done_file.write(line)
+done_file.write('\n\n\nTime to Complete 5-8 Hours')
---
+++ test.py
@@ -0,0 +1,44 @@
+import os
+from selenium import webdriver
+from axe_selenium_python import Axe
+
+print('Enter the site you would like to test')
+target = input().strip()
+print('Select folder name')
+foldername = input()
+
+if 'https://' in target:
+    no_http_target = target.lstrip('https://')
+elif 'http://' in target:
+    no_http_target = target.lstrip('http://')
+else:
+    no_http_target = target
+if no_http_target.endswith('/'):
+    t_filename = no_http_target.rstrip('/')
+else:
+    t_filename = no_http_target.replace("/", "_")
+
+if '.' in t_filename:
+    no_dot_filename = t_filename.replace('.', '_')
+else:
+    no_dot_filename = t_filename
+
+if not os.path.exists(foldername):
+    os.makedirs(foldername)
+target_file = open(f'{foldername}/{no_dot_filename}-a11y.txt', 'w')
+target_file.write(f'Page tested: {target}')
+
+driver = webdriver.Chrome()
+print(target)
+driver.get(target)
+axe = Axe(driver)
+axe.inject()
+results = axe.run()
+for result in results["violations"]:
+    target_file.write(f'\n\nDescription:\n{result["description"]}\n')
+    target_file.write(f'Help URL:\n{result["helpUrl"]}\n\n')
+    target_file.write("Messages:\n")
+    for node in result["nodes"]:
+        for n_any in node["any"]:
+            target_file.write(f'{n_any["message"]}\n')
+driver.close()
